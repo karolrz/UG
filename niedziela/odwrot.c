d1466f136288a4384915e5dcc5a04bcf54857789
Karol Rzepka <krzepka@p39-14.labpk.inf.ug.edu.pl>
2012-10-28 12:57:11 +0100

    dodanie zad7

---
+++ niedziela/odwrot.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() 
+{
+
+int a, b, c;
+
+  printf("Podaj kolejno trzy liczby:\n");
+
+  printf("Pierwsza: ");  scanf("%d",&a);
+
+  printf("Druga: ");  scanf("%d",&b);
+
+  printf("Trzecia:");  scanf("%d",&c);
+
+
+  printf("Wpisaleś %d, %d, %d \n",a,b,c);
+printf("Dodawanie" );
+
+  return 0;
+}
+
